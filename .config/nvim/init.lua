ad7d076193cd8c1dbc1d967d3ef64853a69698d4
od00bi <>
2023-08-29 21:42:24 +0200

    nvim reconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,45 @@
--- This combo lists current line on current line and all other are relative
+require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    use 'AlexvZyl/nordic.nvim'
+    use 'Mofiqul/dracula.nvim'
+end)
+
+vim.cmd.colorscheme 'dracula'
+
+local builtin = require("telescope.builtin")
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
+    auto_install = true,
+    highlight = { enable = true }
+}
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-l><C-l>", '<cmd>set invrelativenumber<CR>')
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
+
 vim.o.number = true
 vim.o.relativenumber = true
-vim.keymap.set('n', '<C-l><C-l>', '<cmd>set invrelativenumber<CR>')
-
--- tabstop: length of tab,
--- expandtab: use spaces instead,
--- shiftwidth: length of indent.
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.autoindent = true
-
--- show hidden chars
+vim.opt.listchars = { eol = "$", trail = "~", multispace = "." }
 vim.o.list = true
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
