bd6ad57899c011082a5d342655bba9530a7b8025
od00bi <>
2023-10-02 19:03:58 +0200

    231002

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,33 +13,55 @@ require('packer').startup(function(use)
         run = ':TSUpdate'
     }
 
-    use 'AlexvZyl/nordic.nvim'
     use 'Mofiqul/dracula.nvim'
 end)
+local vim = vim
+local cmd = vim.cmd
+local keymap = vim.keymap
+local opt = vim.opt
 
-vim.cmd.colorscheme 'dracula'
+cmd.colorscheme 'dracula'
 
 local builtin = require("telescope.builtin")
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
     auto_install = true,
-    highlight = { enable = true }
+    highlight = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "gnc",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        }
+    }
 }
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-l><C-l>", '<cmd>set invrelativenumber<CR>')
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
+keymap.set("n", "<leader>pv", cmd.Ex)
+keymap.set("n", "<C-l><C-l>", '<cmd>set invrelativenumber<CR>')
+keymap.set("n", "<leader>ff", builtin.find_files, {})
+keymap.set("n", "<leader>gf", builtin.git_files, {})
+keymap.set("n", "<leader>bf", builtin.buffers, {})
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.autoindent = true
-vim.opt.listchars = { eol = "$", trail = "~", multispace = "." }
-vim.o.list = true
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.listchars = { eol = "$", trail = "~", multispace = "." }
+opt.list = true
+opt.foldmethod= "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldenable = false
 
+
+--keymap.set("n", "", builtin.live_grep)
+keymap.set("n", "<leader>lg", builtin.live_grep)
+keymap.set("n", "<leader>gs", builtin.grep_string)
+-- keymap.set("n", "", builtin.live_grep)
+-- keymap.set("n", "", builtin.live_grep)
+-- keymap.set("n", "", builtin.live_grep)
